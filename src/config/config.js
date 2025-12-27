930891994d729c7cff206546fba5f6fb494dee5b
JhonnyMv16 <67910069+JhonnyMv16@users.noreply.github.com>
2025-12-27 11:02:24 -0300

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -9,7 +9,7 @@ const config = {
     endLat: process.env.endLat || null,
     endLng: process.env.endLng || null,
     earthRadius: 6371000, // Raio da Terra em metros
-    maxDistance: 1000000, // 1.000 km
+    maxDistance: 5000, // 1.000 km
     valhallaUrl: 'https://valhalla1.openstreetmap.de/route',
     boundingBoxes: [
         [-6.008113207697144, -65.33976311858397, 4.785817506182841, -36.687421712194116],  // Norte/Nordeste BR
